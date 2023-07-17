d29767445c6e835898107220fb795b3b1bb6ee18
Riley B <30447344+rbro3551@users.noreply.github.com>
2023-07-16 22:26:52 -0500

    Initial Commit

---
+++ CrypList/CrypListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypListApp.swift
+//  CrypList
+//
+//  Created by Riley Brookins on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CrypListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
